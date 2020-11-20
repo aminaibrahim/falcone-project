6d8801a8039dba121110d40a24f73890f688249a
Amina Ibrahim <aminaibrahim790@gmail.com>
2020-11-21 02:31:38 +0530

    updated function names

--- src/app/components/planet-selection/planet-selection.component.ts
+++ src/app/components/planet-selection/planet-selection.component.ts
@@ -9,43 +9,37 @@ import { planet, PlanetsApiService } from 'src/app/services/planets-api/planets-
 })
 export class PlanetSelectionComponent implements OnInit {
   choiceList=[1,2,3,4]
+  allPlanetsList = new Array;
+  availablePlanetsList = new Array;
 
-  planetsList;
-  filteredList;
+  selectedPlanets = new Array;
+  
   
   constructor(private planetService: PlanetsApiService) { }
 
   ngOnInit(): void {
     this.getPlanetsList();
   }
-  selectedPlanets = new Array;
   
+
   getPlanetsList() {
     this.planetService.getPlanetsList()
       .subscribe((data: planet[]) =>{
-
-        // this.amina = data.filter((element)=>
-        //   this.selectedPlanets.indexOf(element.name)!==1)
-        // console.log(this.planetsList);
-        this.filteredList=data;
-        this.planetsList = data;
+        this.allPlanetsList = data;
+        this.availablePlanetsList=data;
+        
       })
 
   }
 
   onOptionsSelected(i:number,value:string){
-    this.planetService.onPlanetSelected();
-    // if(this.selectedPlanets[i]!
-    // this.selectedPlanets.splice(i,0,value);
-    this.selectedPlanets[i]=value;
-    console.log(this.selectedPlanets);
     
-     this.planetsList = this.filteredList.filter(element=>{
-       console.log(element);
-       
-       console.log(this.selectedPlanets.indexOf(element.name)==-1);
-       return (this.selectedPlanets.indexOf(element.name)==-1)
-   
-})
+
+    this.planetService.onPlanetSelected();
+    
+    this.selectedPlanets[i]=value;
+    
+     this.availablePlanetsList = this.allPlanetsList.filter(element=> (this.selectedPlanets.indexOf(element.name)==-1)
+   )
 }
 }
