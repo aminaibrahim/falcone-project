c6887ac2d0148d69d066b9ef81f9d9c6cc119097
Amina Ibrahim <aminaibrahim790@gmail.com>
2020-11-21 15:12:23 +0530

    vehicle-select update

--- src/app/components/vehicle-selection/vehicle-selection.component.ts
+++ src/app/components/vehicle-selection/vehicle-selection.component.ts
@@ -11,27 +11,52 @@ export class VehicleSelectionComponent implements OnInit {
 
   selectedPlanets = new Array;
   choices = [1,2,3,4]
-  vehicleList;
+  vehiclesList :vehicle[]= new Array;
+  selectedVehicles:vehicle[]= new Array;
+  availableVehicles = new Array;
   constructor(private planetService: PlanetsApiService,
     private vehicleService: VehiclesAPIService) { }
 
   ngOnInit(): void {
+console.log('amina');
+this.getVehiclesList();
 
     this.planetService.isPlanetSelected.subscribe((data)=>{
       this.selectedPlanets = data;
-        this.getVehiclesList();
       
     })
    
   }
-  onItemChange(i,value){
-    console.log(" Value is : ",i, value );
+  onItemChange(index,value){
+    console.log(" Value is : ",index, value );
+    this.selectedVehicles[index]= value;
+    console.log(this.selectedVehicles);
+    this.amina[index]=this.availableVehicles.map((element,i)=>{
+    
+      
+        if (value===element.name) {
+          return {
+            ...element,
+            total_no: element.total_no -1
+          }
+        }
+        else {
+          return element
+        }
+      
+      }).filter((element=>element.total_no!==0))
+console.log(this.availableVehicles);
+
+    
+    
  }
 
   getVehiclesList() {
     this.vehicleService.getVehiclesListFromAPI()
-      .subscribe((data: vehicle[]) => 
-        this.vehicleList = data)
+      .subscribe((data: vehicle[]) => {
+        this.vehiclesList = data;
+        this.availableVehicles = data;
+      })
   }
 
 }
